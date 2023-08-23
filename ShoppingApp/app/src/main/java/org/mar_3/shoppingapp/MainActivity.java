6dc3905f6fe2f30a2b45172a23009b2409712989
Mar-3 <=>
2023-08-23 20:06:59 +0300

    Navigation drawer and first main fragment done

---
+++ ShoppingApp/app/src/main/java/org/mar_3/shoppingapp/MainActivity.java
@@ -0,0 +1,50 @@
+package org.mar_3.shoppingapp;
+
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.view.MenuItem;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // Used as reference for navigation drawer
+        // https://www.geeksforgeeks.org/navigation-drawer-in-android/
+
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragmentContainerView, FirstFragment.class, null)
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
